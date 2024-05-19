faccbc470a5bf619788474a325d47f01737461ed
cbanish <cbanish.balakrishnan@gmail.com>
2024-05-19 10:13:11 +0530

    Hackerrank solution: DiagonalDiff

---
+++ src/hackerrank/DiagonalDiff.java
@@ -0,0 +1,16 @@
+package hackerrank;
+
+import java.util.*;
+
+public class DiagonalDiff {
+    public static int diagonalDifference(List<List<Integer>> arr) {
+        for (List<Integer> item : arr) {
+
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
